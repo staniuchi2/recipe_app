9361cef7507c4007d60721d5c4aadb536bc9bef2
staniuchi2 <seiyataniuchi@yahoo.co.jp>
2024-04-29 15:08:01 +0100

    Website now displays recipe names from db

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template, request
+from database_system.core import core_get_connection
+from mysql.connector import connect, Error
+
 app = Flask(__name__)
+
+conn = core_get_connection()
+cursor = conn.cursor()
+
+def refresh_recipe_list():
+    query = """SELECT recipe_id, recipe_name
+            FROM test_db_name.recipes"""
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
+
 
 @app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+    recipe_list = refresh_recipe_list()
+    return render_template('index.html', recipes = recipe_list)
 
 
-@app.route("/recipes/<recipe_id>")
+@app.route("/recipes/<recipe_id>", methods = ['GET','POST'])
 def recipes(recipe_id):
-    return render_template("recipes.html")
+
+    return render_template("recipes.html", recipe = recipe_id)
