956590d868108a535d18891dcbc54a66247423fa
Nathan <nathanfenwick@rocketmail.com>
2024-05-05 13:16:27 +0100

    Adjusted base.js to have a drop-down menu for the account button.

--- client/src/base.js
+++ client/src/base.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const Base = ({ children }) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [buttonWidth, setButtonWidth] = useState(null);
+    const buttonRef = useRef(null);
+
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+        // Set button width on click, ensures the ref is current
+        if (buttonRef.current) {
+            setButtonWidth(buttonRef.current.offsetWidth);
+        }
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div style={{ minHeight: '100vh' }}>
             <AppBar position="static" style={{ backgroundColor: '#0a141e' }}>
@@ -17,7 +37,37 @@ const Base = ({ children }) => {
                         </Link>
                     </Typography>
                     <Button style={{ marginRight: 20 }} variant="text" color="inherit" component={Link} to="/recipe/new">New Recipe</Button>
-                    <Button style={{ marginRight: 20 }} variant="outlined" color="inherit" component={Link} to="/user">Account</Button>
+                    <div>
+                        <Button
+                            ref={buttonRef}
+                            variant="outlined"
+                            color="inherit"
+                            id="fade-button"
+                            aria-controls={open ? 'basic-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}
+                        >
+                            Account
+                        </Button>
+                        <Menu
+                            id="fade-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                'aria-labelledby': 'basic-button',
+                            }}
+                            PaperProps={{
+                                style: {
+                                    width: buttonWidth,
+                                },
+                            }}
+                        >
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+                    </div>
                 </Toolbar>
             </AppBar>
             <main>{children}</main>
